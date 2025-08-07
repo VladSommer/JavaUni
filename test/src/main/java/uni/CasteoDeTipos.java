77fed8ef8bd6e4bc036f54b028baaff27b633f11
VladSommer <vladsommer2024@gmail.com>
2025-08-06 22:24:34 -0300

    Update CasteoDeTipos.java

--- test/src/main/java/uni/CasteoDeTipos.java
+++ test/src/main/java/uni/CasteoDeTipos.java
@@ -5,14 +5,18 @@ import java.util.Scanner;
 public class CasteoDeTipos {
     public static void main(String[] args) {
         Scanner n1_input = new Scanner(System.in);
+        // int n1 = n1_input.nextInt();
         double n1 = n1_input.nextDouble();
         Scanner n2_input = new Scanner(System.in);
+        // int n2 = n2_input.nextInt();
         double n2 = n2_input.nextDouble();
-        
+
+        // int res = n1 / n2;
         double res = n1 / n2;
         
         System.out.println(res);
     }
 }
+
 
 
